ffa24f7224b10313936d32352569b68a98f039ce
Daniel Zen <daniel@zendigital.com>
2012-02-21 13:52:03 -0500

    Extracted _class function 1st draft

--- src/MyObject.js
+++ src/MyObject.js
@@ -2,60 +2,36 @@
 // http://www.klauskomenda.com/code/javascript-programming-patterns/#module
 // Modeled after jquery definition. Still searching for my ideal object definition pattern
 
-var MyObject = (function () {
-  var MyObject = function (state) {
-//    var obj = new MyObject();
-    return new MyObject.fn.init(state);
+var _class = function (cls) {
+  var classObj = function (args) {
+    return new classObj.fn.init(args);
   };
 
-  MyObject.fn = MyObject.prototype = {
-    constructor: MyObject,
-    init: function(state) {
-      this.setState(state);
-      return this;
-    },
-    setState:function (state) {
-      this._state = state;
-    },
-    addState:function (state) {
-      if (typeof this._state === "string") {
-        this._state = [this._state, state];
-      } else {
-        this._state.push(state);
-      }
-    },
-    getState:function () {
-      return this._state;
+  classObj.fn = classObj.prototype = cls;
+  classObj.fn.init.prototype = classObj.fn;
+  return classObj;
+
+  //  Can I create a revealing pattern with this? Hide fn?
+};
+
+var MyObject = _class({
+//  constructor:this,
+  init:function (state) {
+    this.setState(state);
+    return this;
+  },
+  setState:function (state) {
+    this._state = state;
+  },
+  addState:function (state) {
+    if (typeof this._state === "string") {
+      this._state = [this._state, state];
+    } else {
+      this._state.push(state);
     }
-  };
-  MyObject.fn.init.prototype = MyObject.fn;
-  return MyObject;
-  //  Can I create a revealing pattern with this?
-  //  {init: init, setState: setState, getState: getState, addState: addState };
-})();
-
-/*
- // Old School
- //
-var MyObject = function (state) {
-  this._state = state;
-}
-
-
-MyObject.prototype.setState = function (state) {
-  this._state = state;
-};
-
-
-MyObject.prototype.addState = function (state) {
-  if (typeof this._state === "string") {
-    this._state = [this._state, state];
-  } else {
-    this._state.push(state);
+  },
+  getState:function () {
+    return this._state;
   }
-};
+});
 
-MyObject.prototype.getState = function () {
-  return this._state;
-}
-*/
