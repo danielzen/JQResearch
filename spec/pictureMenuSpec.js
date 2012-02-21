bf3c1ef97d9ee732a9624274e31ce41a6bc35af4
Daniel Zen <daniel@zendigital.com>
2012-02-21 14:32:40 -0500

    pictureMenu with tests

--- spec/pictureMenuSpec.js
+++ spec/pictureMenuSpec.js
@@ -1,11 +1,26 @@
 describe("Picture Menu", function () {
 
-  describe("show Picture menu", function () {
-    it("should add picture menu & pink-fade divs inside tag", function () {
-      var div = $("<div/>");
-      showPictureMenu(div);
-      expect(div.children.length).toBe(2);
-    });
+  var div = $("<div/>");
+  showPictureMenu(div);
+  var picMenu = div.children('div.picture-menu');
 
+  describe("show Picture menu", function () {
+    it("should add picture-menu & pink-fade divs inside tag", function () {
+      expect(div.children('div').length).toBe(2);
+      expect(picMenu.length).toBe(1);
+      expect(div.children('div.pink-fade').length).toBe(1);
+    });
+    it("should have setting and delete imgs inside picture menu", function() {
+      expect(picMenu.children.length).toBe(2);
+      expect(picMenu.children('img').length).toBe(2);
+    });
+  });
+
+  describe("hide Picture menu", function () {
+    it("should remove the picture-menu & pink-fade divs inside tag", function () {
+      expect(div.children('div').length).toBe(2);
+      hidePictureMenu(div);
+      expect(div.children('div').length).toBe(0);
+    });
   });
 });
--- src/pictureMenu.js
+++ src/pictureMenu.js
@@ -1,11 +1,12 @@
 
-var hidePictureMenu = function() {
-  $('.pink-fade').remove();
-  $('.picture-menu').remove();
-}
-
 var showPictureMenu = function(item){
   var fade = $("<div class='pink-fade' />").css({ opacity: 0.4});
-  $(item).append(fade); //.mouseleave(function () { console.log("out"); hidePictureMenu(); });
+  $(item).append(fade);
   $(item).append($('<div class="picture-menu"><img src="../img/settings.gif" alt="settings"><img src="../img/delete.gif" alt="delete"></div>'));
-}
+};
+
+var hidePictureMenu = function (item) {
+  var jqItem = $(item);
+  jqItem.find('.pink-fade').remove();
+  jqItem.find('.picture-menu').remove();
+};
