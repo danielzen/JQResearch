628d85eeb1fde4b2634569a15a9a7ef2bd2f90f1
Daniel Zen <daniel@zendigital.com>
2012-02-26 17:56:38 -0500

    first attempt at angular widget, not yet functioning

--- src/picture.js
+++ src/picture.js
@@ -13,6 +13,29 @@ var Picture = _class({
   }
 })
 
+angular.widget('cb:picture', function(compileElement){
+  var compiler = this;
+  compileElement.css('display', 'block');
+  var urlExp = compileElement.attr('url');
+  var nameExp = compileElement.attr('name');
+  return function(linkElement){
+    var image = angular.element('<img src="'+urlExp+'" alt="'+nameExp+'">');
+    var position = angular.element('<span>'+1+'</span>');
+    var selected = angular.element('<div class="selected" style="visibility: hidden; "></div>');
+    var picMenu = angular.element('<div class="picture-menu" style="visibility: hidden; "><img src="../img/settings.gif" alt="?"><img src="../img/delete.gif" alt="x"></div>');
+    linkElement.append(image);
+    linkElement.append(position);
+    linkElement.append(selected);
+    linkElement.append(picMenu);
+    this.$watch(urlExp, function(value){
+      image.text(value);
+    });
+    this.$watch(nameExp, function(value){
+      image.text(value);
+    });
+  };
+});
+
 var appendPicturesToPortfolio = function(pictures, portfolio) {
   var vertical = "<li class='vertical'/>";
   var length = pictures.length;
